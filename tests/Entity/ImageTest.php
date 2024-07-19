5bc627663782e59d3dbf6298019b47fbf1503172
Nicolas <nicolas.castex.pro@gmail.com>
2024-07-19 22:47:32 +0200

    update test for image

--- tests/Entity/ImageTest.php
+++ tests/Entity/ImageTest.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\Image;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ImageTest extends TestCase
 {
+
     public function testImage(): void
     {
         $image = new Image();
         $image->setName('angora.png');
+        $image->setImageFile(new UploadedFile('../Fixture/ch.jpg','ch.jpg'));
 
         $this->assertEquals($image->getName(), 'angora.png');
+        $this->assertEquals($image->getImageFile()->getFilename(), 'ch.jpg');
     }
 }
