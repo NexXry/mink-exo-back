b3fdd17b74fe58a53deae001e51d28cf1bbe4313
Nicolas Castex <nicolas.castex.pro@gmail.com>
2024-07-24 21:53:56 +0200

    README.md + fix test

--- tests/Entity/ImageTest.php
+++ tests/Entity/ImageTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests\Entity;
 
 use App\Entity\Image;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -16,7 +15,7 @@ class ImageTest extends TestCase
     {
         $image = new Image();
         $image->setName('angora.png');
-        $image->setImageFile(new UploadedFile('../Fixture/ch.jpg','ch.jpg'));
+        $image->setImageFile(new UploadedFile(__DIR__ . '\Fixture\ch.jpg', 'ch.jpg'));
 
         $this->assertEquals($image->getName(), 'angora.png');
         $this->assertEquals($image->getImageFile()->getFilename(), 'ch.jpg');
